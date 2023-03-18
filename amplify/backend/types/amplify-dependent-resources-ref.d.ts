565f83eec5fc32241c5a42ddd56178835716ded9
Uday Kiran Kavaturu <udaykiran.kavaturu@xcubelabs.com>
2023-03-18 06:25:09 +0530

    up to authentication backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "sppreactea1cfbea": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
